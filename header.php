881a6f40eca9cc27db2d50e538fb3011649d64f1
Giphan <giphankipngeno@gmail.com>
2026-03-09 12:17:31 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <header class="site-header">
     <div class="header-content">
-        <a href="index.php" class="brand" style="text-decoration:none;"><h1>The Blue Reviewer</h1></a>
+        <a href="index.php" class="brand" style="text-decoration:none;">
+            <h1>The Blue Reviewer</h1>
+        </a>
         <div class="nav-right">
             <a href="#newsletter" class="cta-btn">Get Best Deals First</a>
         </div>
