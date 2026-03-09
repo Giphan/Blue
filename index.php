43ec28cab565fa880a44a72ad82e8728e9e08e06
Giphan <giphankipngeno@gmail.com>
2026-03-09 12:32:36 +0300

    Add toggleMenu function for navigation menu

--- index.php
+++ index.php
@@ -42,5 +42,15 @@
 
 <?php include('footer.php'); ?>
 
+<script>
+function toggleMenu() {
+    const menu = document.getElementById('navMenu');
+    if(menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+}
+</script>
 </body>
 </html>
